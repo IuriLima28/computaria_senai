5a8703b94ce7fd1abd9539d67e414ad1b288dfe2
Iuri d'Oliveira de Lima <doliveiraiuri@gmail.com>
2025-04-15 16:47:20 -0300

    Add files via upload

---
+++ graus.py
@@ -0,0 +1,5 @@
+celsius = float(input("Digite a temperatura em Celsius: "))
+kelvin = celsius + 273.15
+fahrenheit = celsius*1.8+32
+
+print(celsius, " graus celsius é ", kelvin, " graus kelvin e ", fahrenheit, " graus fahrenheit.")
