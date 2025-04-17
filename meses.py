9f43855df99ca20605e58ff0c12938ab4466aedf
Iuri d'Oliveira de Lima <doliveiraiuri@gmail.com>
2025-04-17 16:33:52 -0300

    Add files via upload

---
+++ meses.py
@@ -0,0 +1,27 @@
+r = int(input("Digite o mes do ano: "))
+if r == 1:
+    print("Janeiro")
+elif r == 2:
+    print("Fevereiro")
+elif r == 3: 
+    print("Março")
+elif r == 4:
+    print("Abril")
+elif r == 5:
+    print("Maio")
+elif r == 6:
+    print("Junho")
+elif r == 7:
+    print("Julho")
+elif r == 8:
+    print("Augosto")
+elif r == 9:
+    print("Setembro")
+elif r == 10:
+    print("Outubro")
+elif r == 11: 
+    print("Novembro")
+elif r == 12:
+    print("Dezembro")
+else:
+    print("Valor inválido")
